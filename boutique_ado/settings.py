361ab6473880bd02729e3112ef2c9980428b264d
Evangelos Alexiou <evansa44@gmail.com>
2024-10-12 13:50:34 +0000

    Fix type os.environ

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environment
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutique-ado-wthrough-ba89b23b2149.herokuapp.com', 'localhost']
 
