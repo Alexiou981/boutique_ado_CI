0fca9430f002035e10f01423be14c9b39f046719
Evangelos Alexiou <evansa44@gmail.com>
2024-10-12 13:56:01 +0000

    Fix typo

--- custom_storages.py
+++ custom_storages.py
@@ -1,4 +1,4 @@
-from django.cof import settings
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
@@ -7,4 +7,4 @@ class StaticStorage(S3Boto3Storage):
 
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+    location = settings.MEDIAFILES_LOCATION
